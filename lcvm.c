5b634e838877feb71cac1ba4f1a656a14570b876
wiljoyaa <wiljoyaa@gmail.com>
2026-03-31 16:15:02 -0400

    ADD case
    Added update flags and sign extend function to be able and implement ADD instruction

--- lcvm.c
+++ lcvm.c
@@ -67,6 +67,21 @@ enum
 uint16_t memory[MEMORY_MAX];
 
 
+uint16_t sign_extend (uint16_t x, int bit_count){
+	if ((x >> (bit_count - 1)) & 1){
+		x |= (0xFFFF << bit_count);
+	}
+	return x;
+}
+
+
+void update_flags (uint16_t){
+	if (reg[r] == 0) {reg[R_COND] = FL_ZRO;} 
+	else if (reg[r] >> 15) {reg[R_COND] = FL_NEG;} 
+	else {reg[R_COND] = FL_POS;}
+}
+
+
 
 // Main Loop
 int main(int argc, const char* argv[]){
@@ -99,7 +114,18 @@ int main(int argc, const char* argv[]){
 
 		switch(op){
 			case OP_ADD: 
-				@{ADD}
+				uint16_t r0 = (instr >> 9) & 0x7;
+				uint16_t r1 = (instr >> 6) & 0x7;
+				uint16_t imm_flag = (instr >> 5) & 0x1;
+
+				if (imm_flag){
+					uint16_t imm5 = sign_extend(instr & 0x1F, 5);
+					reg[r0] = reg[r1] + imm5;
+				} else {
+					uint16_t r2 = instr & 0x7;
+					reg[r0] = reg[r1] + reg[r2];
+				}
+				update_flags(r0);
 				break;
 
 			case OP_AND:
